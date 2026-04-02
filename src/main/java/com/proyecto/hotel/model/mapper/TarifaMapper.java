4e5f506b03dd85b28d743e9061c1951af98bdd7b
josue <estebanph_1003@hotmail.com>
2026-04-02 14:27:01 -0500

    fix: mapeo explícito en TarifaMapper para evitar unmapped target

--- src/main/java/com/proyecto/hotel/model/mapper/TarifaMapper.java
+++ src/main/java/com/proyecto/hotel/model/mapper/TarifaMapper.java
@@ -8,7 +8,12 @@ import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", uses = {TipoHabitacionMapper.class, TipoAlquilerMapper.class})
 public interface TarifaMapper {
+    @Mapping(source = "tipoHabitacion.id", target = "tipoHabitacionId")
+    @Mapping(source = "tipoAlquiler.id", target = "tipoAlquilerId")
     TarifaDTO toDTO(Tarifa tarifa);
+
+    @Mapping(target = "tipoHabitacion", ignore = true)
+    @Mapping(target = "tipoAlquiler", ignore = true)
     Tarifa toEntity(TarifaDTO tarifaDTO);
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "tipoHabitacion", ignore = true)
